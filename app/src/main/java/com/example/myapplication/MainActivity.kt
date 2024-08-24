1a80451fff7c41bd75a4b42345e46409ad5abebd
Grover <grovergonzalez8@gmail.com>
2024-08-23 20:29:38 -0400

    comentando errores

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -18,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val breakWorld = Button(this).apply {
+        /*val breakWorld = Button(this).apply {
             text = "Break the world"
             setOnClickListener {
                 Sentry.captureException(RuntimeException("This app uses Sentry! :)"))
@@ -27,7 +26,7 @@ class MainActivity : ComponentActivity() {
 
         addContentView(breakWorld, ViewGroup.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        )
+        )*/
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
